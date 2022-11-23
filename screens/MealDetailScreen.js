f6f2796a51b0b98d243ca2eef6b9aa0319b4465c
Tony Greeley <tonygreeley@Tonys-iMac.local>
2022-11-23 12:28:31 -0600

    Completed basic app without state management so favorties is not functional.

---
+++ screens/MealDetailScreen.js
@@ -0,0 +1,88 @@
+import { useLayoutEffect } from 'react';
+import {
+  ScrollView,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+} from 'react-native';
+import List from '../components/MealDetail/List';
+import Subtitle from '../components/MealDetail/Subtitle';
+import MealDetails from '../components/MealDetails';
+import { MEALS } from '../data/dummy-data';
+import IconButton from '../components/IconButton';
+
+const MealDetailScreen = ({ route, navigation }) => {
+  const mealId = route.params.mealId;
+
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  const headerButtonPressHandler = () => {
+    console.log('Pressed');
+  };
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <IconButton
+            icon='star'
+            color='white'
+            title='Tap Me!'
+            onPress={headerButtonPressHandler}
+          />
+        );
+      },
+    });
+  }, [navigation, headerButtonPressHandler]);
+
+  return (
+    <ScrollView style={styles.rootContainer}>
+      <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+      <Text style={styles.title}>{selectedMeal.title}</Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        affordability={selectedMeal.affordability}
+        complexity={selectedMeal.complexity}
+        textStyle={styles.detailText}
+      />
+      <View style={styles.listOuterContainer}>
+        <View style={styles.listContainer}>
+          <Subtitle>Ingredients:</Subtitle>
+          <List data={selectedMeal.ingredients} />
+          <Subtitle>Steps:</Subtitle>
+          <List data={selectedMeal.steps} />
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default MealDetailScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    marginBottom: 32,
+  },
+  image: {
+    width: '100%',
+    height: 350,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    margin: 8,
+    textAlign: 'center',
+    color: 'white',
+  },
+  detailText: {
+    color: 'white',
+  },
+  listOuterContainer: {
+    alignItems: 'center',
+  },
+  listContainer: {
+    width: '80%',
+  },
+});
---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,66 @@
+import { useLayoutEffect } from 'react';
+
+import { FlatList, StyleSheet, View } from 'react-native';
+import { MEALS, CATEGORIES } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
+
+const MealsOverviewScreen = ({ route, navigation }) => {
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]);
+
+  const renderMealItem = (itemData) => {
+    const item = itemData.item;
+
+    const mealItemProps = {
+      id: item.id,
+      title: item.title,
+      imageUrl: item.imageUrl,
+      duration: item.duration,
+      complexity: item.complexity,
+      affordability: item.affordability,
+    };
+
+    return (
+      <MealItem
+        id={mealItemProps.id}
+        title={mealItemProps.title}
+        imageUrl={mealItemProps.imageUrl}
+        duration={mealItemProps.duration}
+        complexity={mealItemProps.complexity}
+        affordability={mealItemProps.affordability}
+      />
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
+    </View>
+  );
+};
+
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
